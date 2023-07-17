733b760f9bddb1c5255abca5f125efcacffcb0dc
JinMu Go <jingmuio@gmail.com>
2023-07-17 16:14:21 +0900

    :pencil2: Chore(ex00): fix a typo

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,11 +6,23 @@
 /*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 17:06:40 by jgo               #+#    #+#             */
-/*   Updated: 2023/07/17 15:40:43 by jgo              ###   ########.fr       */
+/*   Updated: 2023/07/17 16:13:57 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
+
+static inline void printWhatEver(void) {
+
+	std::cout << " __      __  __                __    ____                             \n";
+	std::cout << "/\\ \\  __/\\ \\/\\ \\              /\\ \\__/\\  _`\\                           \n";
+	std::cout << "\\ \\ \\/\\ \\ \\ \\ \\ \\___      __  \\ \\ ,_\\ \\ \\L\\_\\  __  __     __   _ __   \n";
+	std::cout << " \\ \\ \\ \\ \\ \\ \\ \\  _ `\\  /'__`\\ \\ \\ \\/\\ \\  _\\L /\\ \\/\\ \\  /'__`\\/\\`'__\\ \n";
+	std::cout << "  \\ \\ \\_/ \\_\\ \\ \\ \\ \\ \\/\\ \\L\\.\\_\\ \\ \\_\\ \\ \\L\\ \\ \\ \\_/ |/\\  __/\\ \\ \\/  \n";
+	std::cout << "   \\ `\\___x___/\\ \\_\\ \\_\\ \\__/.\\_\\\\ \\__\\\\ \\____/\\ \\___/ \\ \\____\\\\ \\_\\  \n";
+	std::cout << "    '\\/__//__/  \\/_/\\/_/\\/__/\\/_/ \\/__/ \\/___/  \\/__/   \\/____/ \\/_/  \n";
+	std::cout << "                                                                      \n";
+}
 
 static inline void defaultTest(void) {
 	int a = 2;
@@ -38,7 +50,7 @@ static inline void myTest(void) {
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
 	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
-	
+
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
@@ -47,9 +59,10 @@ static inline void myTest(void) {
 }
 
 int main(void) {
-	std::cout << "===== defaultTest =====" << std::endl;
+	printWhatEver();
+	std::cout << "===== defaultTest =====\n" << std::endl;
 	defaultTest();
-	std::cout << "===== myTest =====" << std::endl;
+	std::cout << "\n===== myTest =====\n" << std::endl;
 	myTest();
 	return 0;
 }
